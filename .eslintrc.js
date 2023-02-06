35327209a3f6305ab4318ae2755c5247dd737472
MoHo Khaleqi <habibsandyfan@gmail.com>
2023-02-06 14:12:44 +0000

    feat: add eslint spell checker plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,38 @@ module.exports = {
     ecmaVersion: 'latest',
     project: 'tsconfig.json'
   },
+  plugins: [
+    'spellcheck'
+  ],
   rules: {
     'for-direction': 2,
-    'no-constant-condition': 1
+    'no-constant-condition': 1,
+    'no-useless-escape': 0,
+    'spellcheck/spell-checker': ['warn',
+      {
+        comments: true,
+        strings: true,
+        identifiers: true,
+        templates: true,
+        lang: 'en_GB',
+        skipWords: [
+          'commitlint',
+          'utils',
+          'ecma',
+          'lang',
+          'plugins'
+        ],
+        skipIfMatch: [
+          'http://[^s]*',
+          '^[-\\w]+\/[-\\w\\.]+$'
+        ],
+        skipWordIfMatch: [
+          '^foobar.*$',
+          'tsconfig*',
+          'es20*'
+        ],
+        minLength: 2
+      }
+    ]
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable spellcheck/spell-checker */
 export const magicalAdd = (num1: number, num2: number): number => {
   return num1 + num2
 }
